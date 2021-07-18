bb13a060cc89ebd731cc43c12d5cae223551bbcb
onepierre <onepierrethe4@gmail.com>
2021-07-18 11:15:46 +0200

    add cardboard crack cartoons

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from image_print import create_image
 import numpy as np
 from random import *
 import xkcd as xkcd
+import os
+import random
 
 CARTOON_MAX = 2488
 BOT_NAME = "Twopierre"
@@ -46,30 +48,46 @@ async def _astrid(ctx):
         await ctx.channel.send(file=picture)
 
 
+@bot.command(name="mtg", pass_context=True)
+async def _mtg(ctx):
+    if "cartoons" in ctx.channel.name:
+        path = random.choice(os.listdir('cardboard_crack/'))
+        name = path.split("_")[0].replace("-"," ")
+        with open('cardboard_crack/' + path, 'rb') as f:
+            picture = discord.File(f)
+            if not name.isdigit() or name == "2021":
+                await ctx.channel.send(name[0].capitalize() + name[1:])
+            await ctx.channel.send(file=picture)
+    
+
 @bot.command(name="xkcd", pass_context=True)
 async def _xkcd(ctx, var="False"):
-    err = False
-    if var.isdigit():
-        nb = int(var)
-        if nb <= 0 or nb > xkcd.getLatestComicNum():
-            err = True
-            await ctx.channel.send("Ce numéro n'est pas valable, réessaye.")
+    if "cartoons" in ctx.channel.name:
+        err = False
+        last_num = xkcd.getLatestComicNum()
+        digit = (var.startswith("-") and var[1:].isdigit()) or var.isdigit()
+        if digit:
+            nb = int(var)
+            if nb > last_num or nb <= -1 * last_num:
+                err = True
+                await ctx.channel.send("Ce numéro n'est pas valable, réessayez.")
+            elif nb > 0:
+                comic = xkcd.getComic(nb)
+            else:
+                comic = xkcd.getComic(last_num + 1 + nb)
+        elif var == "last":
+            comic = xkcd.getLatestComic()
         else:
-            comic = xkcd.getComic(nb)
-    elif var == "last":
-        comic = xkcd.getLatestComic()
-    else:
-        comic = xkcd.getRandomComic()
-    if not err:
-        await ctx.channel.send(comic.getTitle())
-        await ctx.channel.send(comic.getImageLink())
-        await ctx.channel.send(comic.getAltText())
-
-
+            comic = xkcd.getRandomComic()
+        if not err:
+            await ctx.channel.send(comic.getTitle())
+            await ctx.channel.send(comic.getImageLink())
+            await ctx.channel.send(comic.getAltText())
 
 @bot.command(name="exit")
 async def _exit(ctx):
-    exit()
+    if ctx.author.name == "Onepierre":
+        exit()
 
 
 
